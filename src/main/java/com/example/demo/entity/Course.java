ea07c96858ca697e655d1edd430e74b9ed9769fe
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-04-27 13:24:09 +0500

    create  class Course

---
+++ src/main/java/com/example/demo/entity/Course.java
@@ -0,0 +1,58 @@
+package com.example.demo.entity;
+
+import java.util.Objects;
+
+public class Course {
+    private long id;
+    private String  name ;
+    private Double cost;
+    private int duration;
+
+    public Course() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Course course = (Course) o;
+        return id == course.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getCost() {
+        return cost;
+    }
+
+    public void setCost(Double cost) {
+        this.cost = cost;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
