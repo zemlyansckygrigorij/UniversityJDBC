9210747294fbb2b43a5fec50617ebfcb10111690
zemlyansckygrigorij <zemlyanscky.grigorij@yandex.ru>
2021-04-27 13:24:38 +0500

    create  class Subject

---
+++ src/main/java/com/example/demo/entity/Subject.java
@@ -0,0 +1,49 @@
+package com.example.demo.entity;
+
+import java.util.Objects;
+
+public class Subject {
+    private long id;
+    private String name;
+    private String description ;
+
+    public Subject() {
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Subject subject = (Subject) o;
+        return id == subject.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
